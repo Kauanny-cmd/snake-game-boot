032727b8ac72555007f6af793ac40fad2fab7492
kauanny-cmd <kauannyvieira8@gmail.com>
2022-03-21 11:45:32 -0300

    criando direções e loop de "entra" e "sai"

--- script.js
+++ script.js
@@ -20,21 +20,45 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown',update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right'){
+        direction = 'left'
+    } else if(event.keyCode == 38 && direction != 'down'){
+        direction = 'up'
+    } else if(event.keyCode == 39 && direction != 'left'){
+        direction = 'right'
+    } else if(event.keyCode == 40 && direction != 'up'){
+        direction = 'down'
+    }
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == 'right'){
+        snake[0].x = 0
+    } else if (snake[0].x < 0 && direction == 'left'){
+        snake[0].x = 16 * box
+    } else if(snake[0].y > 15 * box && direction == 'down'){
+        snake[0].y = 0
+    } else if(snake[0].y < 0 && direction == 'up'){
+        snake[0].y = 16 * box
+    }
+
     criarBG();
     criarCobrinha();
 
     let snakeX = snake[0].x;
-    let snakeY = snake[0].Y;
+    let snakeY = snake[0].y;
 
     if(direction == 'right'){
        snakeX += box; 
     } else if(direction == 'left'){
         snakeX -= box;
     } else if(direction == 'up'){
-        snakeY += box;
-    } else{
-        snakeY -= box}
+        snakeY -= box;
+    } else if(direction == 'down'){
+        snakeY += box}
 
     snake.pop();
 
